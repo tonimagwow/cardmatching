ef98ce4db76b91bae3f36383a549b582fb3bc239
Antionio Magana <antoniomagana@Antionios-MacBook-Air.local>
2020-04-04 04:11:08 -0700

    cardmatching

--- app.js
+++ app.js
@@ -1,57 +1,103 @@
-console.log ('It Works?')
+// console.log("hello world")
 
-//variables for levels and cards? 
-let cardFlipped = false
-let firstClick;
-let secondClick;
-let deck = $('.card')
-// let level1 = ('level 1')
-// let level2 = ('level 2')
-// let level3 = ('level 3')
+let clickedCard = null;
+let disableCard = false; //will stop the click of a card to stop the clicking of cards
+let cardMatch = 0;
 
-$(() => {
-    //make the game function to start the game on level one, with 8 cards (maybe add more cards), add a timer of 30sec, if you beat the timer move on to level 2, if you don't beat the timer, alert that they lost, prompt to play again or quit
-    $('.card').click(turnCard) 
-    function turnCard () {
-        // console.log("clicked")
-        // console.log(this)
-        this.classList.toggle('rotate');
 
-        if(!cardFlipped) {
-            cardFlipped = true;
-            firstClick = this;
+function flipOver(event) { //using the onclick callback
+    const target = event.currentTarget;
+       
+    if (disableCard ||
+        target === clickedCard || 
+        target.className.includes('done')) {
+            return;
+        }
+        target.className = target.className
+        .replace('color', 'done')
+       
+    
+    // console.log(target.getAttribute('id'));
 
-            // console.log({cardFlipped, firstClick});
+    if (!clickedCard) { //if the card has not been selected       
+        clickedCard = target;  
+    } else if (clickedCard) {
+        //if the clicked 
+        if (clickedCard.getAttribute('id') !== target.getAttribute('id')) {
+                disableCard = true;
+                
+                setTimeout(() => { 
+                console.log('Not a match');
+                clickedCard.className =
+                clickedCard.className.replace('done', 'color')
+                target.className =
+                target.className.replace('done', 'color')  
+                clickedCard = null;
+                disableCard = false;
+            }, 1000);
         } else {
-            cardFlipped = false;
-            secondClick = this;
-
-            //console.log({firstClick, secondClick})
+            cardMatch++; //add 1 to card match until user reaches goal of 8 pair.
+            clickedCard = null;
+            if (cardMatch === 8) {
+                let message = document.getElementById('winner-message');
+                message.classList.remove('winner')
+            }
         }
     }
+    // function reset() {
+    // let restartGame = document.getElementById('btn')
+    // if()
+    //     clickedCard = null,
+    //     disableCard = false,
+    //     cardMatch = 0 
+    // };
         
-    $('.card').click(shuffleCards)
-    function shuffleCards() {
-        
-    }
+}
+
+
+
+
+// let cardIdArray = ['red', 'red', 'blue', 'blue', 'green', 'green', 'yellow', 'yellow', 'pink', 'pink', 'orange', 'orange', 'black', 'black', 'brown', 'brown']
+// let cardId = document.getElementsByClassName('card');
+// let cardIdArray = document.getElementsByClassName('card')
+// let cardId = [...cardIdArray];
+// let cardElements = document.getElementsByClassName('card');
+// let cardElementsArray = [...cardElements];
+
+// console.log(cardIdArray)
+
+//////////////
+//Start Game//
+//////////////
+// function startGame(array) {
+
+//     let cardIdArray = ['red', 'red', 'blue', 'blue', 'green', 'green', 'yellow', 'yellow', 'pink', 'pink', 'orange', 'orange', 'black', 'black', 'brown', 'brown']
+
+//     for (let i = array.length - 1; i > 0; i--) {
+//         let j = Math.floor(Math.random() * 1);
+//         let temp = array[i];
+//         array[i] = array[i];
+//         array[j] = temp;
+//     }
+//     return;
     
+//     // var random = 0;
+//     //   var temp = 0;
+//     //   for (i = 1; i < cardId.length; i++) {
+//     //     random = Math.round(Math.random() * i);
+//     //     temp = cardId[i];
+//     //     cardId[i] = cardId[random];
+//     //     cardId[random] = temp;
+//     //     cardId[i].appendChild(.appendChild())
+//     //   }
+//     // for(let i = cardIdArray.length - 1; i > 0; i--) {
+//     //     let randomCard = Math.floor(Math.random() * (i * 1));
+//     //     this.cardIdArray[randomCard]
+//     // }
 
 
-
-
-    //add a scoreboard to keep track of score through out levels
-
-
-    //level 2 add 16 cards to the game board, timer of 20sec to beat, if they win more to level 3, if they lose, alert they've lost and ask them to quit or restart.
-
-
-    //level 3 add 24 cards, timer set to 15sec (maybe 20), if they win, prompt that they've beat the game.
-    
-    
-    //possibly make a level that has one timer that continues to count down from 60sec and goes through all three levels? 
-
-
-
-
-
-})
+//     // let shuffledCards = shuffle(cardIdArray);
+//     // for (let i = 0; i < shuffledCards.length; i--) {
+//     //     cardId[i].appendChild(shuffledCards[i]);
+//     // }
+// };
